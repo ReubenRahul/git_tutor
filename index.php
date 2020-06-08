7a6c037efbd68b22f13b78b0e1c94f96e0fd7223
ReubenRahul <reuben.rahul@ithands.biz>
2020-06-08 14:17:55 +0530

    resolve

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
 <?php
-echo 'index.php';
-echo 'index2.php';
-echo 'in feature brnahc'
-echo 'revet this soon';
-echo 'revert this time';
-echo 'from hotfix1';
-echo 'from hotfix2';
-echo 'from feature2';
-echo 'form hotfix2 third'
-echo 'from hotfix3';
-
-echo 'from teting2';
