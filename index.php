6ef48a2679a63fa316a029e76cfa3376c0e9f37e
Reuben Rahul <reuben.rahul@ithands.biz>
2020-07-08 18:48:04 +0530

    commit 1

--- index.php
+++ index.php
@@ -1,22 +1,2 @@
 <?php
-
-function utils()
-{
-	switch ($type) {
-		case 'COUNT':
-			# count cod here
-			break;
-		case 'KG':
-		 #return KG unit
-		 break;	
-		 case 'POUND':
-		  # pound code here	
-		   break;
-		  case 'INCH':
-		  #inch returns
-		  break;
-		default:
-			# code...
-			break;
-	}
-}
+changes from feat/test branch commit 1
