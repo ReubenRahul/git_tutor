de05a25492ccaffb7ae25e0dd4c1a5974a718b06
ReubenRahul <reuben.rahul@ithands.biz>
2020-07-20 22:04:29 +0530

    master function

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
 
-function utils()
-{
-	switch ($type) {
-		case 'COUNT':
-			# count cod here
-			break;
-		case 'KG':
-		 #return KG unit
-		 break;	
-		 case 'POUND':
-		  # pound code here	
-		   break;
-		  case 'INCH':
-		  #inch returns
-		  break;
-		default:
-			# code...
-			break;
-	}
-}
-echo 'hello india';
+
+ function master()
+ {
+
+ }
+
+
+ function branch2()
+ {
+
+ }
+
+ function branch3()
+ {
+ 	
+ }
